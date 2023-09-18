5d31f9beca16d4f3e49ecd1163028f8e2545da31
kylead10 <adiokayode111@gmail.com>
2023-09-18 16:52:10 +0100

    a single page react website inspired by netflix movie page

---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav_black'}`}>
+      <img
+        className='nav_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
+        alt='Netflix Logo'
+      />
+      <img
+        className='nav_avatar'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        alt='Netflix Logo'
+      />
+    </div>
+  );
+};
+
+export default Nav;
